8c95ae3aa8f4365708e8e7ca12b0a10fc17cb9b5
Magnus Leßmann <github@m-lessmann.de>
2020-01-13 22:17:10 +0100

    Add a collection of seeds to be tested

---
+++ docker/python/run_as_approaches.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+import time
+
+seedSource = open("./test_seeds.txt", "r")
+
+print("Spawning endpoint.")
+sPort = int(os.getenv("SERVER_PORT", "50123"))
+sICTestApp = os.getenv("IC20_LOCATION", "./ic20_windows.exe")
+runCount = 0
+proc = subprocess.Popen(["/usr/bin/env", "python3", "./main.py"])
+time.sleep(2)
+
+while True:
+    currentSeed: str = seedSource.readline()
+    currentSeed = currentSeed.strip('\n\r ')
+    if not currentSeed or not currentSeed.strip():
+        break
+
+    runCount += 1
+    print(f'Run {runCount} with seed {currentSeed} against port {sPort}')
+    icProc = subprocess.Popen([sICTestApp, '-s', currentSeed, '-u', "http://localhost:" + str(sPort) + "/"])
+    icProc.wait()
+    if icProc.returncode != 0:
+        print(f'Execution failed.')
+        proc.kill()
+        exit(1)
+
+proc.kill()
